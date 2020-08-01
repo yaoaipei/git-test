f82711ebfd9d75a447fb04e24de3fe7b71f7fd90
yaoaipei <2231397740@qq.com>
2020-08-02 02:05:32 +0800

    把本地添加到git

---
+++ gittest/src/com/demo/git/gitclass.java
@@ -0,0 +1,17 @@
+package com.demo.git;
+
+public class gitclass {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
